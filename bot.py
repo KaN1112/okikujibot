6a1ca3ca64be631466fceb2764c230f6ec227fe3
KaN1112 <takosdonfe@gmail.com>
2026-06-26 21:00:13 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,7 +99,7 @@ async def on_ready():
     await bot.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.watching,
-            name="!omikuji"
+            name="おみくじ引くには→!omikuji"
         )
     )
 
